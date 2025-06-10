56090cf3124048ac1ed2bb624010d86d953d7944
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-06-10 13:24:19 +0200

    feat: send while invariants file through telegram

--- src/main/java/es/us/isa/restest/bot/oracle/OracleIdentifierBot.java
+++ src/main/java/es/us/isa/restest/bot/oracle/OracleIdentifierBot.java
@@ -9,23 +9,34 @@ import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Identifies test oracles (invariants) from a collection of test case executions.
+ *
+ * <p>This bot operates in a stateless manner to support parallel execution. It uses the file system
+ * as the source of truth:
+ *
+ * <ul>
+ *   <li>The number of accumulated test cases is determined by counting the lines of the generated
+ *       Beet input file.
+ *   <li>A marker file ({@code .invariants_identified}) is created in the experiment's data
+ *       directory upon successful invariant identification to prevent redundant analysis.
+ * </ul>
+ */
 public class OracleIdentifierBot extends BaseBot {
   private static final String DAIKON_JAVA_PATH = System.getenv("DAIKON_JAVA_PATH");
   private static final String DAIKON_PATH = System.getenv("DAIKON_EXEC_PATH");
-  private static final File DAIKON_OUTPUT_FILE = new File("invariants.csv");
+  private static final String INVARIANTS_FILE_NAME = "invariants.csv";
+  private static final String INVARIANTS_IDENTIFIED_MARKER = ".invariants_identified";
 
   private static final Logger log = LoggerFactory.getLogger(OracleIdentifierBot.class);
 
   private int minTestCases;
-  private final Map<String, Integer> accumulatedTestCases = new HashMap<>(); // TODO: parallelize
 
   @Override
   public void configure() {
@@ -36,21 +47,20 @@ public class OracleIdentifierBot extends BaseBot {
   }
 
   @OrderHandler("analyze_invariants")
-  public void analyzeInvariants(JSONObject message) throws IOException, InterruptedException {
+  public void analyzeInvariants(JSONObject message) throws IOException {
     String restestConfigPath = message.getString("userConfigPath");
     String batchId = message.getString("batchId");
-    int batchSize = message.getInt("batchSize");
-
-    int totalTestCases = this.accumulatedTestCases.getOrDefault(restestConfigPath, 0) + batchSize;
-
-    if (totalTestCases < 0) {
-      // test oracles already identified
-      return;
-    }
 
     RESTestLoader loader = new RESTestLoader(restestConfigPath);
     Path testDataDirectory =
         Path.of(loader.readProperty("data.tests.dir"), loader.getExperimentName());
+
+    Path markerFile = testDataDirectory.resolve(INVARIANTS_IDENTIFIED_MARKER);
+    if (Files.exists(markerFile)) {
+      log.info(
+          "Invariants already identified for {}, skipping analysis.", loader.getExperimentName());
+      return;
+    }
 
     File testCasesFile =
         testDataDirectory
@@ -69,60 +79,92 @@ public class OracleIdentifierBot extends BaseBot {
     log.info("Adding test results to the Beet input file...");
     BeetInputFileGenerator.convertFromRestest(testCasesFile, testResultsFile, beetInputFilePath);
 
-    if (totalTestCases >= this.minTestCases) {
-      log.info("Reached minimum amount of test cases to identify invariants!");
-      this.identifyTestOracles(loader, testCasesFile, beetInputFilePath);
-      this.accumulatedTestCases.put(restestConfigPath, -1);
-      return;
-    }
+    long totalTestCases = countLines(beetInputFilePath);
+    log.info("Total accumulated test cases for {}: {}", loader.getExperimentName(), totalTestCases);
 
-    this.accumulatedTestCases.put(restestConfigPath, totalTestCases);
+    try {
+      if (totalTestCases >= this.minTestCases) {
+        log.info("Reached minimum amount of test cases to identify invariants!");
+        Files.createFile(markerFile);
+        this.identifyTestOracles(loader, testDataDirectory, beetInputFilePath);
+      }
+    } catch (Exception e) {
+      this.notifyError(
+          "There was an error while identifying test oracles for %s: %s"
+              .formatted(loader.getExperimentName(), e.toString()));
+    }
   }
 
   private void identifyTestOracles(
-      RESTestLoader restestLoader, File testCasesFile, Path beetInputFilePath)
+      RESTestLoader restestLoader, Path testDataDirectory, Path beetInputFilePath)
       throws IOException, InterruptedException {
     log.info("Running Beet for {}...", restestLoader.getExperimentName());
     runBeet(restestLoader.getSpec().getPath(), beetInputFilePath.toString());
-    log.info("Running Daikon...");
-    runDaikon(testCasesFile);
 
-    String invariants = Files.readString(DAIKON_OUTPUT_FILE.toPath());
+    Path daikonOutputFilePath = testDataDirectory.resolve(INVARIANTS_FILE_NAME);
+    log.info("Running Daikon...");
+    runDaikon(beetInputFilePath.toFile(), daikonOutputFilePath);
+
+    String invariants = Files.readString(daikonOutputFilePath);
     log.info("Invariants detected successfully: \n{}", invariants);
 
-    this.notifyUser(
-        "Identified some invariants for the %s API!\n\n%s"
-            .formatted(restestLoader.getExperimentName(), invariants));
+    this.notifyInvariants(
+        daikonOutputFilePath,
+        "Some invariants in the %s API were found.".formatted(restestLoader.getExperimentName()));
   }
 
   private static void runBeet(String... args) {
     GenerateInstrumentation.main(args);
   }
 
-  private static void runDaikon(File testCasesFile) throws IOException, InterruptedException {
+  private static void runDaikon(File beetInputFile, Path daikonOutputFile)
+      throws IOException, InterruptedException {
     List<String> command =
         List.of(
             DAIKON_JAVA_PATH,
             "-cp",
             DAIKON_PATH,
             "daikon.Daikon",
-            BeetHelper.getDeclsFilePath(testCasesFile),
-            BeetHelper.getDtraceFilePath(testCasesFile));
+            BeetHelper.getDeclsFilePath(beetInputFile),
+            BeetHelper.getDtraceFilePath(beetInputFile));
 
     Process process =
         new ProcessBuilder(command)
-            .redirectOutput(DAIKON_OUTPUT_FILE)
+            .redirectOutput(daikonOutputFile.toFile())
             .redirectError(Redirect.INHERIT)
             .start();
 
     int exitCode = process.waitFor();
     if (exitCode != 0) {
-      throw new RuntimeException("Daikon failed with exit code" + exitCode);
+      throw new RuntimeException("Daikon failed with exit code " + exitCode);
     }
   }
 
-  private void notifyUser(String message) {
+  private void notifyInvariants(Path invariantsFilePath, String caption) {
     this.publishOrder(
-        "telegram_bot", "broadcast_message", String.format("[%s] %s", this.getBotId(), message));
+        "telegram_bot",
+        "send_document",
+        new JSONObject()
+            .put("recipient", "broadcast")
+            .put("type", "document")
+            .put("localPath", invariantsFilePath.toString())
+            .put("caption", String.format("[%s] %s", this.getBotId(), caption))
+            .toString());
+  }
+
+  private void notifyError(String content) {
+    this.publishOrder(
+        "telegram_bot",
+        "send_message",
+        new JSONObject().put("recipient", "broadcast").put("content", content).toString());
+  }
+
+  private long countLines(Path path) throws IOException {
+    if (!Files.exists(path)) {
+      return 0L;
+    }
+    try (var lines = Files.lines(path)) {
+      return lines.count() - 1;
+    }
   }
 }
